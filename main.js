409044dcb5dde245f3b8daeae69c37ada06bb892
Cliff Gunn <clifton.gunn@schneider-electric.com>
2016-03-29 23:53:24 -0500

    Got the initial dependencies installed and some of the starter files created.

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
 /**
  * Created by SESA300553 on 3/29/2016.
  */
+
+// main.js
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+ReactDOM.render(
+<h1>Hello, world!</h1>,
+    document.getElementById('example')
+);
